a6d474fb2d345bfaa44aee3c63cf7a131db5db98
Field-May <mfield-may@MC111580.local>
2019-07-15 10:40:45 +0100

    Add meta tags with helmet

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Helmet } from 'react-helmet'
 import { fetchUsers } from '../actions'
 
 class UsersListPageBase extends Component {
@@ -13,9 +13,19 @@ class UsersListPageBase extends Component {
         return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
     }
 
+    head() {
+        return (
+            <Helmet>
+                <title>{`${this.props.users.length} Users Loaded`}</title>
+                <meta property="og:title" content="Users App" />
+            </Helmet>
+        )
+    }
+
     render () {
         return (
             <div>
+               {this.head()}
                 Here's a list of users
                 <ul>
                     {this.renderUsers()}
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -4,7 +4,7 @@ import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import serialize from 'serialize-javascript';
-
+import { Helmet } from 'react-helmet'
 import { Routes } from '../client/Routes'
 
 export const renderer = (req, store, context) => {
@@ -14,12 +14,16 @@ export const renderer = (req, store, context) => {
                 <div>{renderRoutes(Routes)}</div>
             </StaticRouter>
         </Provider>);
+
+    const helmet = Helmet.renderStatic()
         
     // node doesn't know how to handle jsx so we need a webpack steup to process this
     // this only returns a static html page. No js makes it through to the browser.
     return `
     <html>
         <head>
+            ${helmet.title.toString()}
+            ${helmet.meta.toString()}
             <!-- Compiled and minified CSS -->
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         </head>
